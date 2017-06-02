1f5a46c6926dbed4f5c19e7ceb457dddb89d0837
Cosmin Vana <cosminvana@outlook.com>
2017-06-02 23:09:04 +0300

    Add RequestProgress .NET 4.5 Library and basic test website

---
+++ src/RequestProgress.Web/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace RequestProgress.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static ProgressTracker progressTracker;
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            progressTracker = new ProgressTracker();
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            progressTracker.TrackRequest(Request);
+        }
+    }
+}
---
+++ src/RequestProgress/ProgressTracker.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace RequestProgress
+{
+    public class ProgressTracker
+    {
+        private readonly ProgressTrackerConfiguration progressTrackerConfiguration;
+
+        private readonly IDictionary<string, TrackedRequest> trackedRequests;
+
+        public ProgressTracker() : this(new ProgressTrackerConfiguration())
+        {
+        }
+
+        public ProgressTracker(ProgressTrackerConfiguration progressTrackerConfiguration)
+        {
+            this.progressTrackerConfiguration = progressTrackerConfiguration;
+            this.trackedRequests = new Dictionary<string, TrackedRequest>();
+        }
+
+        public void TrackRequest(HttpRequest request)
+        {
+            var requestIdentifier = request[progressTrackerConfiguration.RequestIdentifierUrlParameterName];
+            if (requestIdentifier != null)
+            {
+                if (!string.IsNullOrEmpty(progressTrackerConfiguration.RetrieveProgressUrl))
+                {
+                    if (request.Url.Segments[request.Url.Segments.Length - 1] == progressTrackerConfiguration.RetrieveProgressUrl)
+                    {
+                        ReturnProgress(requestIdentifier, request);
+                        return;
+                    }
+                }
+
+                var contentLengthHeader = request.Headers[Constants.ContentLengthHeaderName];
+
+                if (contentLengthHeader != null)
+                {
+                    if (!trackedRequests.ContainsKey(requestIdentifier))
+                    {
+                        var contentLength = int.Parse(contentLengthHeader);
+                        var trackedRequest = new TrackedRequest() { LoadedBytes = 0, TotalBytes = contentLength };
+                        trackedRequests.Add(requestIdentifier, trackedRequest);
+                        TrackProgress(request, trackedRequest, requestIdentifier);
+                    }
+                }
+            }
+        }
+
+        private void ReturnProgress(string requestIdentifier, HttpRequest request)
+        {
+            var progress = GetProgress(requestIdentifier);
+
+            var response = request.RequestContext.HttpContext.Response;
+            response.Cache.SetExpires(DateTime.UtcNow.AddDays(-1));
+            response.Cache.SetValidUntilExpires(false);
+            response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+            response.Cache.SetCacheability(HttpCacheability.NoCache);
+            response.Cache.SetNoStore();
+
+            response.Write(progress);
+            response.Flush();
+            response.End();
+        }
+
+        public int GetProgress(string requestId)
+        {
+            if (this.trackedRequests.ContainsKey(requestId))
+            {
+                var trackedRequest = this.trackedRequests[requestId];
+                return trackedRequest.GetPercentage();
+            }
+            return -1;
+        }
+
+        private void TrackProgress(HttpRequest request, TrackedRequest trackedRequest, string requestIdentifier)
+        {
+            var requestStream = request.GetBufferedInputStream();
+
+            byte[] buffer = new byte[progressTrackerConfiguration.ChunkSize];
+
+            int readBytes = requestStream.Read(buffer, 0, progressTrackerConfiguration.ChunkSize);
+            while (readBytes > 0)
+            {
+                trackedRequest.LoadedBytes += readBytes;
+                readBytes = requestStream.Read(buffer, 0, progressTrackerConfiguration.ChunkSize);
+            }
+        }
+    }
+}
